5ded6b9b2f69d31e19325e15c420b6cdd6fdfdd0
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-07-13 13:53:41 +0200

    Added stream and stream factory

--- src/Factory/StreamFactory.php
+++ src/Factory/StreamFactory.php
@@ -1,15 +1,44 @@
 <?php
-declare(strict_types=1);
+
+declare (strict_types = 1);
+
 namespace Nyholm\Psr7\Factory;
 
-
+use Nyholm\Psr7\Stream;
 use Psr\Http\Message\StreamInterface;
 
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
 class StreamFactory implements \Http\Message\StreamFactory
 {
     public function createStream($body = null)
     {
-        // TODO: Implement createStream() method.
-    }
+        if (is_scalar($body)) {
+            // Copy to our stream
+            $stream = fopen('php://temp', 'r+');
+            if ($body !== '') {
+                fwrite($stream, $body);
+                fseek($stream, 0);
+            }
 
+            return new Stream($stream);
+        }
+
+        switch (gettype($body)) {
+            case 'resource':
+                return new Stream($body);
+            case 'object':
+                if ($body instanceof StreamInterface) {
+                    return $body;
+                } elseif (method_exists($body, '__toString')) {
+                    return $this->createStream((string) $body);
+                }
+                break;
+            case 'NULL':
+                return new Stream(fopen('php://temp', 'r+'));
+        }
+
+        throw new \InvalidArgumentException('Invalid resource type: '.gettype($body));
+    }
 }
--- src/Stream.php
+++ src/Stream.php
@@ -1,84 +1,231 @@
 <?php
-declare(strict_types=1);
+
+declare (strict_types = 1);
+
 namespace Nyholm\Psr7;
 
 use Psr\Http\Message\StreamInterface;
 
-class Uri implements StreamInterface
+class Stream implements StreamInterface
 {
+    /**
+     * A resource reference.
+     *
+     * @var int
+     */
+    private $stream;
+
+    /**
+     * @var bool
+     */
+    private $seekable;
+
+    /**
+     * @var bool
+     */
+    private $readable;
+
+    /**
+     * @var bool
+     */
+    private $writable;
+
+    /**
+     * @var array|mixed|null|void
+     */
+    private $uri;
+
+    /**
+     * @var int
+     */
+    private $size;
+
+    /** @var array Hash of readable and writable stream types */
+    private static $readWriteHash = [
+        'read' => [
+            'r' => true, 'w+' => true, 'r+' => true, 'x+' => true, 'c+' => true,
+            'rb' => true, 'w+b' => true, 'r+b' => true, 'x+b' => true,
+            'c+b' => true, 'rt' => true, 'w+t' => true, 'r+t' => true,
+            'x+t' => true, 'c+t' => true, 'a+' => true,
+        ],
+        'write' => [
+            'w' => true, 'w+' => true, 'rw' => true, 'r+' => true, 'x+' => true,
+            'c+' => true, 'wb' => true, 'w+b' => true, 'r+b' => true,
+            'x+b' => true, 'c+b' => true, 'w+t' => true, 'r+t' => true,
+            'x+t' => true, 'c+t' => true, 'a' => true, 'a+' => true,
+        ],
+    ];
+
+    /**
+     * @param int $stream
+     */
+    public function __construct($stream)
+    {
+        if (!is_resource($stream)) {
+            throw new \InvalidArgumentException('Stream must be a resource');
+        }
+
+        $this->stream = $stream;
+        $meta = stream_get_meta_data($this->stream);
+        $this->seekable = $meta['seekable'];
+        $this->readable = isset(self::$readWriteHash['read'][$meta['mode']]);
+        $this->writable = isset(self::$readWriteHash['write'][$meta['mode']]);
+        $this->uri = $this->getMetadata('uri');
+    }
+
     public function __toString()
     {
-        // TODO: Implement __toString() method.
+        try {
+            $this->seek(0);
+
+            return (string) stream_get_contents($this->stream);
+        } catch (\Exception $e) {
+            return '';
+        }
     }
 
     public function close()
     {
-        // TODO: Implement close() method.
+        if (isset($this->stream)) {
+            if (is_resource($this->stream)) {
+                fclose($this->stream);
+            }
+            $this->detach();
+        }
     }
 
     public function detach()
     {
-        // TODO: Implement detach() method.
+        if (!isset($this->stream)) {
+            return;
+        }
+
+        $result = $this->stream;
+        unset($this->stream);
+        $this->size = $this->uri = null;
+        $this->readable = $this->writable = $this->seekable = false;
+
+        return $result;
     }
 
     public function getSize()
     {
-        // TODO: Implement getSize() method.
+        if ($this->size !== null) {
+            return $this->size;
+        }
+
+        if (!isset($this->stream)) {
+            return;
+        }
+
+        // Clear the stat cache if the stream has a URI
+        if ($this->uri) {
+            clearstatcache(true, $this->uri);
+        }
+
+        $stats = fstat($this->stream);
+        if (isset($stats['size'])) {
+            $this->size = $stats['size'];
+
+            return $this->size;
+        }
+
+        return;
     }
 
     public function tell()
     {
-        // TODO: Implement tell() method.
+        $result = ftell($this->stream);
+
+        if ($result === false) {
+            throw new \RuntimeException('Unable to determine stream position');
+        }
+
+        return $result;
     }
 
     public function eof()
     {
-        // TODO: Implement eof() method.
+        return !$this->stream || feof($this->stream);
     }
 
     public function isSeekable()
     {
-        // TODO: Implement isSeekable() method.
+        return $this->seekable;
     }
 
     public function seek($offset, $whence = SEEK_SET)
     {
-        // TODO: Implement seek() method.
+        if (!$this->seekable) {
+            throw new \RuntimeException('Stream is not seekable');
+        } elseif (fseek($this->stream, $offset, $whence) === -1) {
+            throw new \RuntimeException('Unable to seek to stream position '
+                .$offset.' with whence '.var_export($whence, true));
+        }
     }
 
     public function rewind()
     {
-        // TODO: Implement rewind() method.
+        $this->seek(0);
     }
 
     public function isWritable()
     {
-        // TODO: Implement isWritable() method.
+        return $this->writable;
     }
 
     public function write($string)
     {
-        // TODO: Implement write() method.
+        if (!$this->writable) {
+            throw new \RuntimeException('Cannot write to a non-writable stream');
+        }
+
+        // We can't know the size after writing anything
+        $this->size = null;
+        $result = fwrite($this->stream, $string);
+
+        if ($result === false) {
+            throw new \RuntimeException('Unable to write to stream');
+        }
+
+        return $result;
     }
 
     public function isReadable()
     {
-        // TODO: Implement isReadable() method.
+        return $this->readable;
     }
 
     public function read($length)
     {
-        // TODO: Implement read() method.
+        if (!$this->readable) {
+            throw new \RuntimeException('Cannot read from non-readable stream');
+        }
+
+        return fread($this->stream, $length);
     }
 
     public function getContents()
     {
-        // TODO: Implement getContents() method.
+        $contents = stream_get_contents($this->stream);
+
+        if ($contents === false) {
+            throw new \RuntimeException('Unable to read stream contents');
+        }
+
+        return $contents;
     }
 
     public function getMetadata($key = null)
     {
-        // TODO: Implement getMetadata() method.
-    }
+        if (!isset($this->stream)) {
+            return $key ? null : [];
+        } elseif (!$key) {
+            return stream_get_meta_data($this->stream);
+        }
 
+        $meta = stream_get_meta_data($this->stream);
+
+        return isset($meta[$key]) ? $meta[$key] : null;
+    }
 }
