db02e7ff6660ecabbff8da5166dab9bbb24cdc5d
Nyholm <tobias.nyholm@gmail.com>
2018-07-22 13:38:26 -0700

    Bugfix

--- src/Response.php
+++ src/Response.php
@@ -137,7 +137,7 @@ class Response implements ResponseInterface
 
         $new = clone $this;
         $new->statusCode = (int) $code;
-        if (empty($reasonPhrase) && isset(self::$phrases[$new->statusCode])) {
+        if ((null === $reasonPhrase || '' === $reasonPhrase) && isset(self::$phrases[$new->statusCode])) {
             $reasonPhrase = self::$phrases[$new->statusCode];
         }
         $new->reasonPhrase = $reasonPhrase;
