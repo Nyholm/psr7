532b43fc7919389b2145318b715569538fcb0161
=jorgediazmontoya <=>
2021-08-08 22:19:14 -0500

    Stupid error
    * Fix the method createStreamFromFile animals >:(

--- src/Factory/Psr17Factory.php
+++ src/Factory/Psr17Factory.php
@@ -37,19 +37,7 @@ class Psr17Factory implements RequestFactoryInterface, ResponseFactoryInterface,
 
     public function createStreamFromFile(string $filename, string $mode = 'r'): StreamInterface
     {
-        if ('' === $filename) {
-            throw new \RuntimeException('Path cannot be empty');
-        }
-
-        if (false === $resource = @\fopen($filename, $mode)) {
-            if ('' === $mode || false === \in_array($mode[0], ['r', 'w', 'a', 'x', 'c'], true)) {
-                throw new \InvalidArgumentException(\sprintf('The mode "%s" is invalid.', $mode));
-            }
-
-            throw new \RuntimeException(\sprintf('The file "%s" cannot be opened: %s', $filename, \error_get_last()['message'] ?? ''));
-        }
-
-        return Stream::create($resource);
+        return Stream::create($filename);
     }
 
     public function createStreamFromResource($resource): StreamInterface
