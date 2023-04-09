d663b084b901dbf113ab79c353343c3c540fc6f7
Nyholm <tobias.nyholm@gmail.com>
2023-04-09 10:21:45 +0200

    Added some more tests

--- tests/UriTest.php
+++ tests/UriTest.php
@@ -31,6 +31,28 @@ class UriTest extends TestCase
     {
         $uri = (new Uri())
             ->withScheme('https')
+            ->withUserInfo('user', 'pass')
+            ->withHost('example.com')
+            ->withPort(8080)
+            ->withPath('/path/123')
+            ->withQuery('q=abc')
+            ->withFragment('test');
+
+        $this->assertSame('https', $uri->getScheme());
+        $this->assertSame('user:pass@example.com:8080', $uri->getAuthority());
+        $this->assertSame('user:pass', $uri->getUserInfo());
+        $this->assertSame('example.com', $uri->getHost());
+        $this->assertSame(8080, $uri->getPort());
+        $this->assertSame('/path/123', $uri->getPath());
+        $this->assertSame('q=abc', $uri->getQuery());
+        $this->assertSame('test', $uri->getFragment());
+        $this->assertSame('https://user:pass@example.com:8080/path/123?q=abc#test', (string) $uri);
+    }
+
+    public function testSupportsUrlEncodedValues()
+    {
+        $uri = (new Uri())
+            ->withScheme('https')
             ->withUserInfo('foo\user%3D=', 'pass%3D=')
             ->withHost('example.com')
             ->withPort(8080)
